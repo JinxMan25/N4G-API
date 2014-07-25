ac38ea960bf4fe7aaa5232c0312f5a4d4eda66ad
Md Samiul Huq <samiulg3@gmail.com>
2014-07-24 22:24:09 -0700

    project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+N4gApi::Application.config.secret_token = '18f949937114d16bc611df019b8dbedf1d712d9fa53a3688180c8024052573ed32d273d41336d5d5f975270cc81ed9702b48d2cf88470f93d7c89a71bd635587'
