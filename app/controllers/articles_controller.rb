ff810aad3e0b73899010dbf36214b48418ade7f6
Md Samiul Huq <samiulg3@gmail.com>
2014-08-16 18:46:16 -0400

    before filter collect all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,20 @@ class ArticlesController < ApplicationController
   private
 
   def get_articles
-  
+
+  url = "http://www.n4g.com/"
+  doc = Nokogiri::HTML(open(url))
+  links = []
+  sources = []
+
+  doc.css(".sl-item-textwrap h1 a").each do |item|
+    if ( item['href'] =~ /\/ads\/(.*)/ ) 
+
+    else
+      links << item.text
+    end
+  puts item.text
+  end
+
   end
 end
