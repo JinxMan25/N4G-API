5605b208a718de9468bd73281fa769c02c7f5d12
Md Samiul Huq <samiulg3@gmail.com>
2014-08-17 20:18:18 -0400

    indentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,20 +7,18 @@ class ArticlesController < ApplicationController
   private
 
   def get_articles
+    url = "http://www.n4g.com/"
+    doc = Nokogiri::HTML(open(url))
+    links = []
+    sources = []
 
-  url = "http://www.n4g.com/"
-  doc = Nokogiri::HTML(open(url))
-  links = []
-  sources = []
+    doc.css(".sl-item-textwrap h1 a").each do |item|
+      if ( item['href'] =~ /\/ads\/(.*)/ ) 
 
-  doc.css(".sl-item-textwrap h1 a").each do |item|
-    if ( item['href'] =~ /\/ads\/(.*)/ ) 
-
-    else
-      links << item.text
+      else
+        links << item.text
+      end
     end
-  puts item.text
   end
 
-  end
 end
