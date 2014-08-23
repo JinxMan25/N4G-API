d6936c43e2e1849e22103fbdc06ec55a289be94d
Md Samiul Huq <samiulg3@gmail.com>
2014-08-22 21:35:58 -0400

    get article title from element

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,13 @@ class ArticlesController < ApplicationController
     url = "http://www.n4g.com/"
     doc = Nokogiri::HTML(open(url))
 
-
     doc.css(".sl-item").each do |item|
-      article_link = item.css("h1 a")
-      if ( item.css("h1 a").first.attr("href") =~ /\/ads\/(.*)/ ) 
-
+      element_link = item.css("h1 a")
+      if (element_link.first.attr("href") =~ /\/ads\/(.*)/ ) 
+      #do nothing
       else
-        links << article_link.text
+        element_title = article_link.text
+        links << element_link.text
       end
     end
   end
