206d6be93e838ac73763c9a80f467086ad192151
Md Samiul Huq <samiulg3@gmail.com>
2014-08-27 11:04:50 -0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 N4gApi::Application.routes.draw do
-  get "index/" :to => "articles_controller#index"
+  get "index/", :to => "articles#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
