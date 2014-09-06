ba929c904dd66caaf570b44276c4e265c3f42d5f
Md Samiul Huq <samiulg3@gmail.com>
2014-09-06 11:33:40 -0400

    changed socket name

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+config.frameworks -= [:active_record, :active_resource, :action_mailer]
 
 # Initialize the rails application
 N4gApi::Application.initialize!
