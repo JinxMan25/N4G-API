8ce03aea8327a0d60a700cd8c48e144efe58bd18
Md Samiul Huq <samiulg3@gmail.com>
2014-09-06 18:36:28 -0400

    reverted socket path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/var/sockets/unicorn.N4G-API.sock"
+listen "/tmp/unicorn.N4G-API.sock"
 worker_processes 2
 timeout 30
