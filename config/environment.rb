db4de2166ade85ee2dcd6ad6059e5e8f83d2b7d6
Md Samiul Huq <samiulg3@gmail.com>
2014-09-06 11:50:02 -0400

    minor change

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-config.frameworks -= [:active_record, :active_resource, :action_mailer]
 
 # Initialize the rails application
 N4gApi::Application.initialize!
