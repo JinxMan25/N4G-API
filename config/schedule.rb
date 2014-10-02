2ed7e82df983488cea26ed91ed6b175018697b8e
Md Samiul Huq <samiulg3@gmail.com>
2014-10-01 21:34:03 -0400

    exec rake task every 1 hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 5.minutes do
   rake "readability_scrape:fetch_article_body"
 end
 
-every 5.hours do
-  rake "readability_scrape:
+every 1.hours do
+  rake "readability_scrape:fetch_5_page_articles_body" 
 end
