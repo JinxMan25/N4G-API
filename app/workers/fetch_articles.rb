4f2ee63ec498cfaebfde46ebe92ae7f900927f60
Md Samiul Huq <samiulg3@gmail.com>
2014-10-09 16:12:27 -0400

    no arguments for sidekiq op

--- app/workers/fetch_articles.rb
+++ app/workers/fetch_articles.rb
@@ -2,17 +2,13 @@ class FetchArticles
   require 'open-uri'
   include Sidekiq::Worker
 
-  def perform(hash, page)
-    if page >= 5
-      #do nothing
-    else
-      #hash = Rails.cache.fetch("n4g/articles/v1")
-      url_array = hash.map { |hash| hash[:link] }
-      url_array.each do |url|
-        url.gsub!(/QUESTION/, "?url=http://")
-        content = Rails.cache.fetch("http://readability.com/m#{url}", :expires_in => 1.day) do
-        html = open("http://readability.com/m#{url}").read
-        end
+  def perform
+    hash = Rails.cache.fetch("n4g/articles/v1")
+    url_array = hash.map { |hash| hash[:link] }
+    url_array.each do |url|
+      url.gsub!(/QUESTION/, "?url=http://")
+      content = Rails.cache.fetch("http://readability.com/m#{url}", :expires_in => 1.day) do
+      html = open("http://readability.com/m#{url}").read
       end
     end
   end
